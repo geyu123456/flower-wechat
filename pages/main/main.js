7176681b19a47df45a979a3a9dbff557f25ec2ee
gyu98 <gyu163425@gmail.com>
2018-08-02 21:58:23 +0800

    add somethings

---
+++ pages/main/main.js
@@ -0,0 +1,31 @@
+Page({
+  data: {
+    flowerList:[{
+      pic:'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg',
+      name:'鲜花1',
+      price:'168'
+
+    },{
+        pic:'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg',
+        name:'鲜花2',
+        price:'168'
+      
+    },{
+        pic:'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg',
+        name:'鲜花3',
+        price:'168'
+    },{
+        pic:'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg',
+        name:'鲜花4',
+        price:'168'
+    }],
+    movies: [
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' },
+      { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg' }
+    ]
+  },
+  onLoad: function () {
+  }
+}) 
