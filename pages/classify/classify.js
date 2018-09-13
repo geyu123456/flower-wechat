c0d5c41d787d1f9c120054911b4a52e2d74568b5
gyu98 <gyu163425@gmail.com>
2018-09-14 00:18:17 +0800

    add classify

---
+++ pages/classify/classify.js
@@ -0,0 +1,31 @@
+var app = getApp()
+var postdata = require('../../utils/localData.js')
+Page({
+  data: {
+    navLeftItems: [],
+    navRightItems: [],
+    curNav: 1,
+    curIndex: 0
+  },
+  onLoad: function() {
+    console.log(postdata.postList.data)
+    this.setData({
+      navLeftItems: postdata.postList.data
+
+      
+    }) ;
+    console.log(this.data.navLeftItems)
+  },
+
+  //事件处理函数
+  switchRightTab: function(e) {
+    console.log(e.target.dataset)
+    let id = e.target.dataset.id,
+      index = parseInt(e.target.dataset.index);
+    this.setData({
+      curNav: id,
+      curIndex: index
+    })
+  }
+
+})
---
+++ utils/localData.js
@@ -0,0 +1,27 @@
+var localData = {
+  "code": "200",
+  "message": "请求成功",
+  "data": [
+    {
+      "id": 1,
+      "index": "花束"
+    },
+    {
+      "id": 2,
+      "index": "花盒"
+    },
+    {
+      "id": 3,
+      "index": "开业花篮"
+    },
+    {
+      "id": 4,
+      "index": "后备箱布置"
+    }
+
+  ]
+
+}
+module.exports={
+  postList:localData
+}
